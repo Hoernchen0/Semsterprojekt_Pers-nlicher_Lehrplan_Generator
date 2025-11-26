0fc11a2e7a7301976c449031a9e64a33ecabbd0a
Robin <rki43461@stud.hs-furtwangen.de>
2025-11-26 14:47:27 +0100

    Man kann nun PDFs hochladen und die Anbindung funktioniert nun mit Azure

--- AI_Integration.py
+++ AI_Integration.py
@@ -3,25 +3,26 @@ from dotenv import load_dotenv
 import os
 import time
 
-client = OpenAI()
+client = OpenAI(
+    base_url= os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY")
+)
 
 input_text="Platzhalter"
 
 conversation = [
-    {"role": "system", "content": "You are a friendly, concise AI assistant."}
+    {"role": "system", "content": "Hilf dem Nutzer einen Lernplan zu erstellen und"
+                                                    "seine Fragen zu Themen zu beantworten"}
 ]
 
 def ask_gpt(user_input):
     # Add user input to the conversation
     conversation.append({"role": "user", "content": user_input})
-    conversation_text = "\n".join(
-        [f"{m['role']}: {m['content']}" for m in conversation]
-    )
     try:
         response = client.responses.create(
-            model="gpt-5-mini",
+            model="gpt-5-chat",
             instructions="Help the User with his studying",
-            input=conversation_text
+            input=conversation
         )
     except Exception as e:
         print("API error:", e)
@@ -31,6 +32,22 @@ def ask_gpt(user_input):
     conversation.append({"role": "assistant", "content": reply})
     print(response.output_text)
     return 1
+
+def upload_pdf(pdf_path):
+    file = client.files.create(
+        file=open(pdf_path, "rb"), #reading + binary
+        purpose="assistants"
+    )
+    conversation.append({"role": "user", "content": file})
+    file_id = file.id
+    conversation.append({
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": "Hier ist meine PDF."},
+            {"type": "input_file", "file_id": file_id}
+        ]
+    })
+    conversation.append({"role": "system", "content": "Nutze die PDF als Grundlage für Zusammenfassungen und den Lernplan"})
 
 if __name__ == "__main__":
     while True:
