fefa2a1b32cfd6caa18cce016d3f87e19ea1f4b1
Oliver <oliverjehle2004@gmail.com>
2025-12-09 17:25:47 +0100

    chore(Login): Add initial login

---
+++ app/LehrplanGenerator/Views/LoginView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LehrplanGenerator.Views;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
