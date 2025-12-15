4a3c5a9bfd6bf5c8cfd65ea547dca0bc115bd1cb
andreas dudin <adu41543@stud.hs-furtwangen.de>
2025-12-15 03:11:11 +0100

    feat: add formatted variables

--- app/LehrplanGenerator/Logic/Models/StudyEntry.cs
+++ app/LehrplanGenerator/Logic/Models/StudyEntry.cs
@@ -5,7 +5,18 @@ namespace LehrplanGenerator.Logic.Models;
 public record StudyEntry(
     Guid EntryId,
     Guid StudyPlanId,
+    DateTime Date,
     int Hour,
     string Topic,
     int DurationMinutes
-);
+)
+{
+
+    public DateTime Start => DateTime.Today.AddHours(Hour);
+    public DateTime End => Start.AddMinutes(DurationMinutes);
+
+    public string DayFormatted => Date.ToString("dd.MM");
+    public string StartFormatted => Start.ToString("HH:mm");
+    public string EndFormatted => End.ToString("HH:mm");
+    public string DurationFormatted => $"{DurationMinutes} Min";
+};
